36300c07d87985b12bad09152125431b1fa534cd
Jay McConnell <jmmccon@amazon.com>
2019-06-25 14:31:29 -0700

    set invocation count properly

--- src/cfn_resource/request.py
+++ src/cfn_resource/request.py
@@ -16,11 +16,14 @@ class RequestContext:  # pylint: disable=too-many-instance-attributes
         self.logical_resource_id = request["requestData"]["logicalResourceId"]
         self.resource_properties = request["requestData"]["resourceProperties"]
         self.system_tags = request["requestData"]["systemTags"]
+        self.stack_tags: dict = {}
         if "stackTags" in request["requestData"].keys():
             self.stack_tags = request["requestData"]["stackTags"]
         self.get_remaining_time_in_millis = context.get_remaining_time_in_millis
+        self.invocation_count = 0
         if request["requestContext"]:
             self.invocation_count = request["requestContext"]["invocation"]
+        self.previous_stack_tags: dict = {}
         if "previousStackTags" in request["requestData"].keys():
             self.previous_stack_tags = request["requestData"]["previousStackTags"]
 
