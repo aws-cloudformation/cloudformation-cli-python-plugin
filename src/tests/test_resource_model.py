3de07dd51ea25e4902cb96af71d5fa1d184d4836
Toby Fleming <tobflem@amazon.com>
2019-09-03 11:07:57 -0700

    Pre-deletions to make re-write review easier

--- src/tests/test_resource_model.py
+++
@@ -1,9 +0,0 @@
-import unittest
-
-from cfn_resource.base_resource_model import BaseResourceModel
-
-
-class TestResourceModel(unittest.TestCase):
-    def test_new(self):
-        model = BaseResourceModel.new()
-        self.assertEqual({}, model.__dict__)
