3a77b316a5ce0cb40aba00b64fa315260a9a0d07
Jay McConnell <jmmccon@amazon.com>
2020-03-03 16:27:57 -0800

    handle Set and Map

--- python/rpdk/python/resolver.py
+++ python/rpdk/python/resolver.py
@@ -36,7 +36,7 @@ def translate_type(resolved_type):
 
 
 def contains_model(resolved_type):
-    if resolved_type.container == ContainerType.LIST:
+    if resolved_type.container in [ContainerType.LIST, ContainerType.SET]:
         return contains_model(resolved_type.type)
     if resolved_type.container == ContainerType.MODEL:
         return True
