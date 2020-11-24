84bf72799ffd5a982fd4db35389df4221d417681
John Tompkins <jotompki@amazon.com>
2020-11-24 15:57:41 -0800

    Fix CI (#143)

--- e2e-test.sh
+++ e2e-test.sh
@@ -2,6 +2,6 @@
 DIR=$(mktemp -d)
 cd "$DIR"
 ls -la
-cfn init -t AWS::Foo::Bar $1 --use-docker
+cfn init -t AWS::Foo::Bar -a RESOURCE $1 --use-docker
 ls -la
 mypy src/aws_foo_bar/ --strict --implicit-reexport
