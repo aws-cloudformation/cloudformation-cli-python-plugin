a2c9a9c3037e6a279eeb1b50cab440f38b20e109
Anton Mokhovikov <ammokhov@amazon.com>
2021-06-24 15:03:19 -0700

    Version Bump: Lib + Plugin

--- src/setup.py
+++ src/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="cloudformation-cli-python-lib",
-    version="2.1.6",
+    version="2.1.7",
     description=__doc__,
     author="Amazon Web Services",
     author_email="aws-cloudformation-developers@amazon.com",
