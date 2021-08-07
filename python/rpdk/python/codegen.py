f51f1b4938e09cb3338db5668e657cefefdf88ec
Anton Mokhovikov <ammokhov@amazon.com>
2021-08-06 22:04:35 -0700

    fix linter issue

--- python/rpdk/python/codegen.py
+++ python/rpdk/python/codegen.py
@@ -170,7 +170,7 @@ class Python36LanguagePlugin(LanguagePlugin):
         LOG.debug("Generate complete")
 
     def _pre_package(self, build_path):
-        f = TemporaryFile("w+b")
+        f = TemporaryFile("w+b")  # pylint: disable=R1732
 
         with zipfile.ZipFile(f, mode="w") as zip_file:
             self._recursive_relative_write(build_path, build_path, zip_file)
