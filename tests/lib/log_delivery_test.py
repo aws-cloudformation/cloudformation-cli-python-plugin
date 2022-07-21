42a4192a2d6188c10bb84aef574a74feb1f0e6ab
thoward <thoward@godaddy.com>
2022-07-21 16:56:20 -0700

    lint pass

--- tests/lib/log_delivery_test.py
+++ tests/lib/log_delivery_test.py
@@ -86,14 +86,16 @@ def setup_patches(mock_logger):
         make_hook_payload(),
         patch_logger,
         patch__get_logger,
-        patch__get_hook_logger
+        patch__get_hook_logger,
     )
 
 
 @pytest.fixture
 def mock_handler_set_formatter():
     patch__set_handler_formatter = patch.object(ProviderLogHandler, "setFormatter")
-    patch__set_hook_handler_formatter = patch.object(HookProviderLogHandler, "setFormatter")
+    patch__set_hook_handler_formatter = patch.object(
+        HookProviderLogHandler, "setFormatter"
+    )
     return patch__set_handler_formatter, patch__set_hook_handler_formatter
 
 
@@ -206,9 +208,12 @@ def test_setup_existing_logger(setup_patches, mock_session):
 
 def test_setup_with_formatter(setup_patches, mock_session, mock_handler_set_formatter):
     payload, _hook_payload, p_logger, p__get_logger, _p__get_hook_logger = setup_patches
-    p__set_handler_formatter, _p__set_hook_handler_formatter = mock_handler_set_formatter
+    (
+        p__set_handler_formatter,
+        _p__set_hook_handler_formatter,
+    ) = mock_handler_set_formatter
     formatter = logging.Formatter()
-    with p_logger as mock_log, p__get_logger as mock_get, p__set_handler_formatter as mock_set_formatter:
+    with p_logger as mock_log, p__get_logger as mock_get, p__set_handler_formatter as mock_set_formatter:  # pylint: disable=C0301  # noqa: B950
         mock_get.return_value = None
         ProviderLogHandler.setup(payload, mock_session, formatter)
     mock_session.client.assert_called_once_with("logs")
@@ -406,11 +411,16 @@ def test_setup_existing_hook_logger(setup_patches, mock_session):
     mock_log.return_value.addHandler.assert_not_called()
 
 
-def test_setup_with_hook_formatter(setup_patches, mock_session, mock_handler_set_formatter):
+def test_setup_with_hook_formatter(
+    setup_patches, mock_session, mock_handler_set_formatter
+):
     _payload, hook_payload, p_logger, p__get_logger, _p__get_hook_logger = setup_patches
-    _p__set_handler_formatter, p__set_hook_handler_formatter = mock_handler_set_formatter
+    (
+        _p__set_handler_formatter,
+        p__set_hook_handler_formatter,
+    ) = mock_handler_set_formatter
     formatter = logging.Formatter()
-    with p_logger as mock_log, p__get_logger as mock_get, p__set_hook_handler_formatter as mock_set_formatter:
+    with p_logger as mock_log, p__get_logger as mock_get, p__set_hook_handler_formatter as mock_set_formatter:  # pylint: disable=C0301  # noqa: B950
         mock_get.return_value = None
         HookProviderLogHandler.setup(hook_payload, mock_session, formatter)
     mock_session.client.assert_called_once_with("logs")
