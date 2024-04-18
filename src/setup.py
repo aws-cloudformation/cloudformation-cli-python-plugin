9d1ab976406b16276e0e7de3cadb06fdc3b2c8e5
saieshwarm <112009130+saieshwarm@users.noreply.github.com>
2024-04-18 10:38:20 -0700

    bump version to 2.1.18 (#273)

--- src/setup.py
+++ src/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="cloudformation-cli-python-lib",
-    version="2.1.17",
+    version="2.1.18",
     description=__doc__,
     author="Amazon Web Services",
     author_email="aws-cloudformation-developers@amazon.com",
